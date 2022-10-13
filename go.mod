9e38ed5347b90dc3c494b800b5741c275ba2e00c
Mickael Kerjean <mickael@kerjean.me>
2022-10-13 21:38:39 +1100

    feature (speed): cache to make things faster

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/creack/pty v1.1.18
 	github.com/gorilla/websocket v1.5.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a
 )
--- handler/middleware.go
+++ handler/middleware.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	. "github.com/mickael-kerjean/virtualshell/common"
+	"github.com/patrickmn/go-cache"
 	"golang.org/x/crypto/ssh"
 	"net"
 	"net/http"
@@ -9,13 +10,14 @@ import (
 )
 
 func Middleware(fn func(res http.ResponseWriter, req *http.Request)) func(res http.ResponseWriter, req *http.Request) {
+	tmpCache := cache.New(5*time.Minute, 10*time.Minute)
 	return func(res http.ResponseWriter, req *http.Request) {
 		startTime := time.Now()
 		username, password, ok := req.BasicAuth()
 		defer func() {
 			Log.Info(
-				"HTTP %dms %s %s",
-				time.Now().Sub(startTime).Milliseconds(),
+				"HTTP %.1fms %s %s",
+				float32(time.Now().Sub(startTime).Microseconds())/1000,
 				func() string {
 					if ok == false {
 						return "anonymous"
@@ -31,20 +33,24 @@ func Middleware(fn func(res http.ResponseWriter, req *http.Request)) func(res ht
 			ErrorPage(res, ErrNotAuthorized, http.StatusUnauthorized)
 			return
 		}
-		client, err := ssh.Dial("tcp", "127.0.0.1:22", &ssh.ClientConfig{
-			User: username,
-			Auth: []ssh.AuthMethod{ssh.Password(password)},
-			HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-				return nil
-			},
-		})
-		if err != nil {
-			Log.Error("sshd authentication error: %s", err.Error())
-			res.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-			ErrorPage(res, ErrNotAuthorized, http.StatusUnauthorized)
-			return
+
+		if val, found := tmpCache.Get(username + ":" + password); found == true && val != true {
+			client, err := ssh.Dial("tcp", "127.0.0.1:22", &ssh.ClientConfig{
+				User: username,
+				Auth: []ssh.AuthMethod{ssh.Password(password)},
+				HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+					return nil
+				},
+			})
+			if err != nil {
+				Log.Error("sshd authentication error: %s", err.Error())
+				res.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+				ErrorPage(res, ErrNotAuthorized, http.StatusUnauthorized)
+				return
+			}
+			client.Close()
+			tmpCache.Set(username+":"+password, true, cache.DefaultExpiration)
 		}
-		client.Close()
 		fn(res, req)
 	}
 }
